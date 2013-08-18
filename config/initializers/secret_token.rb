7137fbafe5ef5d4a3376a996e18268c6976b7b15
Jörg Thalheim <joerg@higgsboson.tk>
2013-08-18 14:33:08 +0200

    cleanly recreate rails project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SidekiqTestProject::Application.config.secret_token = '8aa0f3d7fe30afadaf497f24dc4e8b1958db9d9652766f0a6317c41302c49818c7fc8b76186d7cbe9ba82d266cb366b0e11c20840f95d111b922ce1074afff95'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SidekiqTestProject::Application.config.secret_key_base = '6dd8b6f9b6f716e7e5647783c2d6553e6c690efaf0be843b5db0c69de20a18638b87401fbe4e88697d6c797f57a2f6803ed54d3eed882d39f9cd8bf648184649'
